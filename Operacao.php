334e3163ede4f25b0b6caaff8870503a23dd512e
Mandarin <pacocadeus48@gmail.com>
2026-08-14 22:20:28 -0300

    Refactor Operacao class structure and methods

--- Operacao.php
+++ Operacao.php
@@ -1,12 +1,8 @@
 <?php
-// Arquivo: Operacao.php
-
 class Operacao {
-    // 1. Atributos (Propriedades Privadas)
     private $valor1;
     private $valor2;
 
-    // Métodos Setters e Getters (Passo 1)
     public function setValor1($valor1) {
         $this->valor1 = $valor1;
     }
@@ -23,27 +19,21 @@ class Operacao {
         return $this->valor2;
     }
 
-    // 2. Métodos de Operações Matemáticas
-
-    // Método para somar (Já fornecido)
     public function somar() {
         $resultado = $this->valor1 + $this->valor2;
         return $resultado;
     }
 
-    // Método para subtrair (Passo 2)
     public function subtrair() {
         $resultado = $this->valor1 - $this->valor2;
         return $resultado;
     }
 
-    // Método para multiplicar (Passo 2)
     public function multiplicar() {
         $resultado = $this->valor1 * $this->valor2;
         return $resultado;
     }
 
-    // Método para dividir (Passo 2)
     public function dividir() {
         if ($this->valor2 == 0) {
             return "Erro: Divisão por zero!";
@@ -52,7 +42,6 @@ class Operacao {
         return $resultado;
     }
 
-    // Método para exponenciar (Passo 3)
     public function exponenciar() {
         $resultado = $this->valor1 ** $this->valor2;
         return $resultado;
